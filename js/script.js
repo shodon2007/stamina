76d76450ad30cd34d04a835a3933931bfb9b4d53
Speed431 <73215393+Speed431@users.noreply.github.com>
2022-12-08 06:34:51 +0700

    added animation on scroll

--- js/script.js
+++ js/script.js
@@ -8,7 +8,39 @@ const home = document.getElementById('home');
 const trainer = document.getElementById('trainer');
 const services = document.getElementById('services');
 const contact = document.getElementById('contact');
+const animItems = document.querySelectorAll('.animItem');
 
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    animOnScroll();
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset)) {
+                animItem.classList.add('active')
+            } else {
+                animItem.classList.remove('active')
+            }
+        }
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+}
 
 
 
@@ -44,7 +76,6 @@ window.addEventListener('scroll', function () {
     } else {
         header.classList.remove('most-scroll');
     }
-
 
 
     //links on header
